1e46b75c97cd3ad2ac0f24b9c874b04ee5e0facd
Garth <112274484+garth0191@users.noreply.github.com>
2024-09-01 13:39:17 -0500

    Update.

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php
+    require '/home/gnmcclur/connections/connect.php';
 
+    //Get username and password from login form.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        //Login.
+        if ((isser($_POST[])))
+    }
+?>
+
+<!DOCTYPE html>
+<html lang = "en">
+    
+</html>
