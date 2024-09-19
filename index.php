b019691ffb7dd8a9c10769458ae4593b7a91880c
Garth <112274484+garth0191@users.noreply.github.com>
2024-09-19 10:20:13 -0500

    Corrections.

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>CSCI 487 Final Project</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link href="index.css" rel="stylesheet">
+        <link rel="stylesheet" href="index.css">
     </head>
     <body>
         <header>
             <!-- Placeholder. Might use custom logo. -->
             <h1 class="heading">CanvasCourse</h1>
+            <h3 class="title">Login or sign-up below!</h3>
         </header>
 
         <!-- Container div. -->
@@ -66,7 +66,6 @@
                 </form>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="index.js"></script>
     </body>
 </html>
