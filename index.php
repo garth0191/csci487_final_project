d255cabe2733f091d86e5679566aeba3cb67e58d
garth0191 <gnmcc91@gmail.com>
2024-09-22 15:14:40 -0500

    Added login and signup functionality to index.php

--- index.php
+++ index.php
@@ -5,6 +5,91 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         //Login.
+        if ((isset($_POST["login_email"]) && $_POST["login_email"] !== "") && (isset($_POST["login_password"]) && $_POST["login_password"] !== "")) {
+            $username = $_POST["login_email"];
+            $password = $_POST["login_password"];
+
+            try {
+                //Prepare SQL query.
+                $stmt = $conn->prepare("SELECT * FROM USER WHERE user_email = :user");
+                $stmt->bindParam(":user", $username);
+                $stmt->execute();
+
+                $isUser = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($isUser && password_verify($password, $isUser["user_password"])) {
+                    //Password is correct.
+                    session_start();
+                    $_SESSION["user_email"] = $isUser["user_email"];
+                    $_SESSION["user_id"] = $isUser["user_id"];
+                    $empty = true;
+                    $message = "";
+
+                    //Check if user is an administrator.
+                    if ($isUser["user_type"] == 0) {
+                        header("Location: admin.php");
+                    } else {
+                        header("Location: home.php");
+                    }
+                } else {
+                    //Password is incorrect.
+                    $empty = false;
+                    $message = "Your password is incorrect. Please try again.";
+                }
+            } catch (PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        } elseif ((isset($_POST["signup_email"]) && $_POST["signup_email"] !== "") && (isset($_POST["signup_password"]) && $_POST["signup_password"] !== "")) {
+            try {
+                $stmt = $conn->prepare("SELECT * FROM USER WHERE user_email = ?");
+                $stmt->execute([$_POST["signup_email"]]);
+
+                //Check that e-mail is not already taken.
+                if ($rowNum = $stmt->rowCount() >= 1) {
+                    $error = true;
+                    $message = "This e-mail is already taken. Please choose a different e-mail, or login.";
+                    header("Location: index.php");
+                }
+
+                $desiredEmail = $_POST["signup_email"];
+                $desiredPassword = $_POST["signup_password"];
+                $desiredUserType = $_POST["type"];
+
+                //Check that submitted passwords match.
+                if ($_POST["signup_password"] !== $_POST["signup_password_confirm"]) {
+                    //Passwords do not match.
+                    $error = true;
+                    $message = "Submitted passwords do not match.";
+                    header("Location: index.php");
+                } else {
+                    $error = false;
+                    $message = "";
+
+                    try {
+                        //Prepare to add user to the database.
+                        $addUserStmt = "INSERT INTO USER (user_email, user_password, user_type) VALUES (?, ?, ?)";
+                        $addUser = $conn->prepare($addUserStmt);
+
+                        //Encrypt password.
+                        $hash = password_hash($desiredPassword, PASSWORD_DEFAULT);
+                        $addUser->execute([$desiredEmail, $hash, $desiredUserType]);
+
+                        $error = false;
+                        $empty = false;
+                        $message = "Registration successful! Please log in.";
+                    } catch (PDOException $e) {
+                        $error = true;
+                        $message = $e->getMessage();
+                        echo "Could not add user to database: " . $e->getMessage();
+                    }
+                }
+            } catch (PDOException $e) {
+                echo "Error retrieving e-mail from database: " . $e->getMessage();
+            }
+        } else {
+            $error = true;
+            $message = "Fields cannot be blank.";
+        }
     }
 ?>
 
@@ -39,7 +124,7 @@
                     <div class="login-container">
                         E-mail: <input type="email" class="email element" placeholder="Input registered e-mail." name="login_email">
                         Password: <input type="password" class="password element" placeholder="Input password." name="login_password">
-                        <?php if($empty) {echo "<div class='error'>".$message."</div>";} ?>
+                        <?php if(!$empty) {echo "<div class='error'>".$message."</div>";} ?>
                         <button class="login-submit">Login</button>
                     </div>
                  </form>
