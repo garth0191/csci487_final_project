6278f4c0609f4dbfbf586babdbe1be5a8f1e4e49
garth0191 <gnmcc91@gmail.com>
2024-09-26 18:18:47 -0500

    Updates.

---
+++ home.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const logoutButton = document.querySelector('.logout');
+
+    if (logoutButton) {
+        logoutButton.addEventListener('click', function() {
+            window.location.href = 'logout.php';
+        })
+    } else {
+        console.error('You have not yet created a LOGOUT button.');
+    }
+});
