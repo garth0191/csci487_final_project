53c01dd3c0dc0c0465083fd4a89edcd8570d72ff
garth0191 <gnmcc91@gmail.com>
2024-09-26 18:02:21 -0500

    Updates.

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+require '/home/gnmcclur/connections/connect.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="mainstyle.css">
+</head>
+
+<body>
+    <header>
+        <h1>
+            <center><img src="./images/logo.png" width="50%" height="50%" alt="CourseCanvas logo"></center>
+        </h1>
+    </header>
+
+<section class="main-section">
+    <div>
+        <center>
+            You did it! You successfully signed it! That means the login functionality is working!
+        </center>
+    </div>
+</section>
+
+<footer class="footer">
+    <p>© Garth McClure. All rights reserved.</p>
+</footer>
+</body>
+</html>
