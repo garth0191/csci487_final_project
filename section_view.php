3a105dc09f7a426a0642853498c6cbde48b6adb3
Garth <112274484+garth0191@users.noreply.github.com>
2024-10-02 10:59:45 -0500

    Updates.

--- section_view.php
+++ section_view.php
@@ -7,10 +7,44 @@ if(!isset($_SESSION['user_id'])){
 }
 
 $user_id = $_SESSION['user_id'];
+$error = false;
 
 // Grab section ID that has been passed to this page.
 if (isset($_GET["section_id"]) && $_GET["section_id"] !== "") {
     $section_id = $_GET["section_id"];
+}
+
+// Check whether an instructor has uploaded a new course item for the section.
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ( (isset($_FILES["course_item"]) && $_FILES["course_item"]["error"] === UPLOAD_ERR_OK ) && (isset($_POST["user_id"]) && $_POST["user_id"] !== "") && (isset($_POST["item_name"]) && $_POST["item_name"] !== "") ) {
+        $item_name = $_POST["item_name"];
+        $instructor_id = $_POST["user_id"];
+
+        try {
+            $item_filepath = $_FILES["course_item"];
+
+            if ($item_filepath['error'] == 0) {
+                //Upload course item to server.
+                $temp_filename = $item_filepath['tmp_name'];
+                $file_extension = pathinfo($item_filepath['name'], PATHINFO_EXTENSION);
+                $new_filename= "USERID_".$instructor_id."_SECTIONID_".$section_id."_".time().".".$file_extension;
+                $upload_path = "course_items/".$new_filename;
+                move_uploaded_file($temp_filename, $upload_path);
+
+                $date = date('Y-m-d');
+
+                try {
+                    //Add course item to database.
+                    $itemAddQuery = $conn->prepare("INSERT INTO ITEM (section_id, item_name, file_path, upload_date) VALUES (?, ?, ?)");
+                    $itemAddQuery->execute([$section_id, $item_name, $upload_path, $date]);
+                } catch (PDOException $e) {
+                    echo "ERROR: Could not add item to database. ".$e->getMessage();
+                }
+            }
+        } catch (PDOException $e) {
+            echo "ERROR: Could not upload course item to server. ".$e->getMessage();
+        }
+    }
 }
 ?>
 
@@ -42,6 +76,7 @@ if (isset($_GET["section_id"]) && $_GET["section_id"] !== "") {
     <!-- Container to hold any items assigned to this section. -->
     <div class="container">
         <div class="main-section">
+            <!-- INSTRUCTORS ONLY: upload new course materials. -->
             <section class="upload-items-form">
                 <h2>Upload Course Items</h2>
                 <form action='section_view.php?id=<?php echo $section_id; ?>' enctype='multipart/form-data' method='post'>
