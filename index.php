1623a029965bb039e6a1c22242cebd1b82a7fb6a
Garth <112274484+garth0191@users.noreply.github.com>
2024-10-07 07:49:49 -0500

    Updates.

--- index.php
+++ index.php
@@ -98,7 +98,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>CSCI 487 Final Project</title>
+        <title>Login/Signup Page</title>
         <link rel="stylesheet" href="index.css">
     </head>
     <body>
