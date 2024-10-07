c5a55473409699f1bab7ad5aced54c63f064e748
Garth <112274484+garth0191@users.noreply.github.com>
2024-10-07 10:59:11 -0500

    Created account_delete.php

---
+++ account_delete.php
@@ -0,0 +1,22 @@
+<?php
+require '/home/gnmcclur/connections/connect.php';
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+//Pull user information from the database.
+try {
+    //Remove user from the database.
+    $deleteQuery = $conn->prepare("DELETE FROM USER WHERE user_id = ?");
+    $deleteQuery->execute([$user_id]);
+    
+} catch (PDOException $e) {
+    echo "ERROR: Could not delete user account.".$e->getMessage();
+}
+
+//Destroy session.
+unset($_SESSION['user_email']);
+unset($_SESSION['user_id']);
+session_destroy();
+header("Location: index.php");
+?>
