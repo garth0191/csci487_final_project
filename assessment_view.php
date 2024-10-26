71dbc6944742598a95a9518281f083a6c58a2a70
garth0191 <gnmcc91@gmail.com>
2024-10-26 09:35:25 -0500

    変更。

--- assessment_view.php
+++ assessment_view.php
@@ -68,7 +68,7 @@
                                     echo "</form>&nbsp;";
                                     echo "<form action='assessment_delete.php?assessment_id=".$oneAssessment["assessment_id"]."' method='post' style='display: inline; padding: 5px;'>";
                                     echo "<input type='hidden' name='course_id' value='".$course_id."'></input>";
-                                    echo "<input type='submit' name='submit' value=' <img src='/images/trash.svg' width='32' height='32'> ' onclick='confirmDelete(event)'></input>";
+                                    echo "<button type='submit' name='submit' onclick='confirmDelete(event)'><img src='/images/trash.svg' width='32' height='32' alt='Delete'></button>";
                                     echo "</form>";
                                     echo "</td>";
 
