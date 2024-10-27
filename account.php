bc8a4d637fa59222e36059f792523c2fddd850b4
garth0191 <gnmcc91@gmail.com>
2024-10-27 10:06:53 -0500

    変更。

--- account.php
+++ account.php
@@ -12,24 +12,6 @@ $error = false;
 $empty = true;
 $message = "";
 
-//Pull user's usertype from database.
-try {
-    $userTypeQuery = $conn->prepare("SELECT * FROM USER WHERE `user_id` = ?");
-    $userTypeQuery->execute([$user_id]);
-    while ($oneUser = $userTypeQuery->fetch(PDO::FETCH_ASSOC)) {
-        $user_type = $oneUser["user_type"];
-        $first_name = $oneUser["first_name"];
-        $last_name = $oneUser["last_name"];
-        $userDescQuery = $conn->prepare("SELECT * FROM USER_TYPE WHERE `type_id` = ?");
-        $userDescQuery->execute([$user_type]);
-        while ($oneDesc = $userDescQuery->fetch(PDO::FETCH_ASSOC)) {
-            $type_description = $oneDesc["type_description"];
-        }
-    }
-} catch (PDOException $e) {
-    echo "ERROR: Could not pull user data from database. ".$e->getMessage();
-}
-
 //Change request.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ((isset($_POST["current_password"]) && $_POST["current_password"] !== "") && (isset($_POST["new_password"]) && $_POST["new_password"] !== "") && (isset($_POST["confirm_password"]) && $_POST["confirm_password"] !== "")) {
@@ -68,25 +50,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message = "Fields must not be blank.";
     }
 
-    if ((isset($_POST["first_name"]) && $_POST["first_name"] !== "")) {
+    if ((isset($_POST["first_name"]) && $_POST["first_name"] !== "") && (isset($_POST["last_name"]) && $_POST["last_name"] !== "")) {
         try {
-            $changeFirstName = $conn->prepare("UPDATE USER SET first_name = ? WHERE `user_id` = ?");
-            $changeFirstName->execute([$_POST["first_name"], $user_id]);
+            $changeName = $conn->prepare("UPDATE USER SET first_name = ?, last_name = ? WHERE `user_id` = ?");
+            $changeName->execute([$_POST["first_name"], $_POST["last_name"], $user_id]);
             $error = false;
         } catch (PDOException $e) {
             echo "ERROR: Could not update first name. ".$e->getMessage();
         }
     }
+}
 
-    if ((isset($_POST["last_name"]) && $_POST["last_name"] !== "")) {
-        try {
-            $changeLastName = $conn->prepare("UPDATE USER SET last_name = ? WHERE `user_id` = ?");
-            $changeLastName->execute([$_POST["last_name"], $user_id]);
-            $error = false;
-        } catch (PDOException $e) {
-            echo "ERROR: Could not update last name. ".$e->getMessage();
+//Pull user's usertype from database.
+try {
+    $userTypeQuery = $conn->prepare("SELECT * FROM USER WHERE `user_id` = ?");
+    $userTypeQuery->execute([$user_id]);
+    while ($oneUser = $userTypeQuery->fetch(PDO::FETCH_ASSOC)) {
+        $user_type = $oneUser["user_type"];
+        $first_name = $oneUser["first_name"];
+        $last_name = $oneUser["last_name"];
+        $userDescQuery = $conn->prepare("SELECT * FROM USER_TYPE WHERE `type_id` = ?");
+        $userDescQuery->execute([$user_type]);
+        while ($oneDesc = $userDescQuery->fetch(PDO::FETCH_ASSOC)) {
+            $type_description = $oneDesc["type_description"];
         }
     }
+} catch (PDOException $e) {
+    echo "ERROR: Could not pull user data from database. ".$e->getMessage();
 }
 ?>
 
