afca117cca97880378c8a52c62c86e7172b3b1fd
Garth <gnmcc91@gmail.com>
2024-11-16 09:44:11 -0600

    Updates.

---
+++ admin_dashboard.js
@@ -0,0 +1,110 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const logoutButton = document.querySelector('.logout');
+    const courseCreateButton = document.querySelector('.create');
+    const homeButton = document.querySelector('.home');
+    const accountButton = document.querySelector('.account');
+    const adminButton = document.querySelector('.admin');
+
+    if (adminButton) {
+        adminButton.addEventListener('click', function() {
+            window.location.href = 'admin_dashboard.php';
+        });
+    } else {
+        console.error('No ADMIN DASHBOARD button is present on the page.');
+    }
+
+    if (logoutButton) {
+        logoutButton.addEventListener('click', function() {
+            window.location.href = 'logout.php';
+        });
+    } else {
+        console.error('You have not yet created a LOGOUT button.');
+    }
+
+    if (courseCreateButton) {
+        courseCreateButton.addEventListener('click', function () {
+            window.location.href = 'course_create.php';
+        });
+    } else {
+        console.error('Could not redirect to course_create.php');
+    }
+
+    if (accountButton) {
+        accountButton.addEventListener('click', function() {
+            window.location.href = 'account.php';
+        });
+    } else {
+        console.error('Could not redirect to account page.');
+    }
+
+    if (homeButton) {
+        homeButton.addEventListener('click', function() {
+            window.location.href = 'home.php';
+        });
+    } else {
+        console.error('Could not redirect to home page.');
+    }
+
+
+    function sortTable(n, tableName) {
+        let table, rows, switching, i, x, y, shouldSwitch, direction;
+        let switchCount = 0;
+
+        table = document.getElementById(tableName);
+        let header = table.getElementsByTagName("th")[n];
+        switching = true;
+
+        const indicators = table.getElementsByClassName("sort-indicator");
+        for (let j = 0; j < indicators.length; j++) {
+            indicators[j].style.display = 'none';
+        }
+
+        // Sorting direction set to ASCENDING.
+        direction = "asc";
+
+        // Loops until no switching has been done.
+        while (switching) {
+            switching = false;
+            rows = table.rows;
+            // Loop through all table rows (except the headers).
+            for (i = 1; i < (rows.length - 1); i++) {
+                shouldSwitch = false;
+                // Comparisons.
+                x = rows[i].getElementsByTagName("td")[n];
+                y = rows[i + 1].getElementsByTagName("td")[n];
+                if (direction == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (direction == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchCount++;
+            } else {
+                if (switchCount == 0 && direction == "asc") {
+                    direction = "desc";
+                    switching = true;
+                }
+            }
+
+            // Show ascending/descending indicator.
+            const indicatorAsc = header.querySelectorAll(".sort-indicator")[0];
+            const indicatorDesc = header.querySelectorAll(".sort-indicator")[1];
+            if (direction === "asc") {
+                indicatorAsc.style.display = 'inline';
+                indicatorDesc.style.display = 'none';
+            } else {
+                indicatorAsc.style.display = 'none';
+                indicatorDesc.style.display = 'inline';
+            }
+        }
+    }
+});
---
+++ admin_dashboard.php
@@ -0,0 +1,118 @@
+<?php
+    require '/home/gnmcclur/connections/connect.php';
+    session_start();
+
+if(!isset($_SESSION['user_id'])){
+    header("Location: index.php");
+}
+
+$user_id = $_SESSION['user_id'];
+$user_type = $_SESSION['user_type'];
+
+// ONLY administrators have access to this page.
+if($user_type != 0){
+    header("Location: home.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard Page</title>
+    <link rel="stylesheet" href="admin_dashboard.css">
+</head>
+
+<body>
+<!-- Nav bar at top of page. -->
+<nav class="navbar">
+    <!-- Will appear on left side of nav bar. -->
+    <div class="navbar-buttons">
+        <div class="button home" id="home-button">Home</div>
+        <!-- Display 'Create Course' option ONLY for instructors. -->
+        <?php
+        if ($user_type < 2) {
+            if ($user_type == 0) {
+                echo "<div class='button admin' id='admin-button'>Admin Dashboard</div>";
+            }
+            echo "<div class='button create' id='create-button'>Create Course</div>";
+        }
+        ?>
+        <div class="button account" id="account-button">Profile</div>
+        <div class="button logout" id="logout-button">Logout</div>
+    </div>
+    <!-- Will appear on right side of nav bar. -->
+    <div class="navbar-logo">
+        <img src="./images/logo.png" height="35%" alt="CourseCanvas logo">
+    </div>
+</nav>
+
+<div class="container">
+    <div class="main-section">
+        <!-- Complete roster of all created courses. -->
+        <h3>Course Listing</h3>
+        <section class="course-list">
+            <table id="course-list-table">
+                <tr>
+                    <th onclick="sortTable(0, 'course-list-table')">Dept ID</th>
+                    <th onclick="sortTable(1, 'course-list-table')">Course Name</th>
+                    <th onclick="sortTable(2, 'course-list-table')">Section</th>
+                    <th onclick="sortTable(3, 'course-list-table')">Semester</th>
+                    <th onclick="sortTable(4, 'course-list-table')">Instructor</th>
+                    <th></th>
+                </tr>
+                <?php
+                    try {
+                        $coursesQuery = $conn->prepare("SELECT * FROM COURSE");
+                        $coursesQuery->execute();
+                        if ($coursesQuery->rowCount() < 1) {
+                            echo "<tr><td colspan='5'><i><b>No created courses exist.</b></i></td></tr>";
+                        } else {
+                            while ($oneCourse = $coursesQuery->fetch(PDO::FETCH_ASSOC)) {
+                                echo "<tr>";
+                                echo "<td>".$oneCourse['course_num']."</td>";
+                                echo "<td>".$oneCourse['course_name']."</td>";
+                                echo "<td>".$oneCourse['course_sec_num']."</td>";
+                                echo "<td>".$oneCourse['semester']."</td>";
+                                echo "<td>".$oneCourse['professor_name']."</td>";
+                                echo "<td>";
+                                    echo "&nbsp;<form action='admin_course_edit?course_id=".$oneCourse["course_id"]."' method='post' style='display: inline; padding: 5px;'>";
+                                    echo "<button type='submit' name='submit' style='background: transparent; border: none; padding: 0; cursor: pointer;'><img src='./images/pencil-square.svg' alt='Edit'></button>";
+                                    echo "</form>&nbsp;";
+                                    echo "<form action='course_delete.php?course_id=".$oneCourse["course_id"]."' method='post' style='display: inline; padding: 5px;'>";
+                                    echo "<button type='submit' name='submit' onclick='confirmDelete(event)' style='background: transparent; border: none; padding: 0; cursor: pointer;'><img src='./images/trash.svg' alt='Delete'></button>";
+                                    echo "</form>";
+                                echo "</td>";
+                                echo "</tr>";
+                            }
+                        }
+                    } catch (PDOException $e) {
+                        echo "ERROR: Could not pull course list from database. ".$e->getMessage();
+                    }
+                ?>
+            </table>
+        </section>
+        <br>
+        <!-- Roster of all users. -->
+        <h3>User Roster</h3>
+        <section class="user-list">
+            <table id="user-list-table">
+                <th onclick="sortTable(0, 'user-list-table')">Last Name</th>
+                <th onclick="sortTable(1, 'user-list-table')">First Name</th>
+                <th onclick="sortTable(2, 'user-list-table')"></th>
+                <th></th>
+            </table>
+        </section>
+    </div>
+</div>
+
+
+<footer class="footer">
+    <p>© Garth McClure. All rights reserved.</p>
+</footer>
+
+<script src="admin_dashboard.js"></script>
+</body>
+</html>
+
