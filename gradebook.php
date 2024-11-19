077ef45b8bdfcaaed1f3232efdd2f511a8a1b500
Garth <gnmcc91@gmail.com>
2024-11-19 08:02:31 -0600

    Updates.

--- gradebook.php
+++ gradebook.php
@@ -161,13 +161,13 @@
                                                 $submission_filepath = null;
                                             }
                                             echo "<td>";
+                                            if ($has_submissions == 1 && !empty($submission_filepath)) {
+                                                echo "<a href='" . htmlspecialchars($submission_filepath, ENT_QUOTES, 'UTF-8') . "' target='_blank'>Submission</a>";
+                                            }
                                             echo htmlspecialchars($score, ENT_QUOTES, 'UTF-8');
 
                                             echo "&nbsp;<button class='edit-grade-button' data-assessment-id='" . $assessment_id . "' data-user-id='" . $oneStudent["user_id"] . "' data-score='" . htmlspecialchars($score, ENT_QUOTES, 'UTF-8') . "' style='background: transparent; border: none; padding: 0; cursor: pointer;'><img src='./images/pencil-square.svg' alt='Edit'></button>";
 
-                                            if ($has_submissions == 1 && !empty($submission_filepath)) {
-                                                echo "<a href='" . htmlspecialchars($submission_filepath, ENT_QUOTES, 'UTF-8') . "' target='_blank'>Submission</a>";
-                                            }
 
                                             echo "</td>";
                                         }
